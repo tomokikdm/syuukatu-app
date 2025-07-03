61e98d282880329f99a2095fb500a66ca22664c2
tomokikdm <kodamatomo0000@gmail.com>
2025-07-04 00:01:18 +0900

    文字変更

--- src/app/company/[id]/layout.tsx
+++ src/app/company/[id]/layout.tsx
@@ -9,7 +9,7 @@ export default async function CompanyLayout({
   params: { id: string };
 }) {
   return (
-    <div className="p-6">
+    <div className="w-full max-w-4xl mx-auto px-4 sm:px-6 py-4 sm:py-6">
       <CompanyTabs id={params.id} />
       {children}
     </div>
